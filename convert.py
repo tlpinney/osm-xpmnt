6dfbf10bf4f16aba57846654f8705e1d9d22faf6
Travis L Pinney <travis.pinney@gmail.com>
2019-02-06 18:26:37 -0500

    Convert changeset bbox to points

---
+++ convert.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('output.tsv') as tsvfile:
+    reader = csv.DictReader(tsvfile, dialect='excel-tab')
+    print("x\ty\tz\tid\n")
+    for row in reader:
+        #print(row)
+        x = ((float(row['min_lon']) + float(row['max_lon']) + 180.0) / 2.0) - 180.0
+        y = ((float(row['min_lat']) + float(row['max_lat']) + 90.0) / 2.0) - 90.0          
+        print(x, y, row['created_at'], row['id'])
+        
+
