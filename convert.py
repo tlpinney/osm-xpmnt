5c9c2c5f45cc95c8ac8c629044d5ed463f1d8dea
Travis L Pinney <travis.pinney@gmail.com>
2019-02-06 18:50:41 -0500

    Fix format

--- convert.py
+++ convert.py
@@ -7,6 +7,6 @@ with open('output.tsv') as tsvfile:
         #print(row)
         x = ((float(row['min_lon']) + float(row['max_lon']) + 180.0) / 2.0) - 180.0
         y = ((float(row['min_lat']) + float(row['max_lat']) + 90.0) / 2.0) - 90.0          
-        print(x, y, row['created_at'], row['id'])
+        print("{}\t{}\t{}\t{}".format(x, y, row['created_at'], row['id']))
         
 
